ce10895b7e0ec3d41c70fa7dabf28eb434bb9e48
unknown <dgmouratorres@gmail.com>
2022-05-11 15:23:56 -0300

    Adicione páginas de falha e de sucesso

--- app.js
+++ app.js
@@ -1,53 +1,60 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const path = require('path')
+const express = require("express");
+const dotenv = require("dotenv").config();
+const path = require("path");
 
-const porta = process.env.PORTA || 3000
+const porta = process.env.PORTA || 3000;
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const mailchimp = require('@mailchimp/mailchimp_marketing')
+const mailchimp = require("@mailchimp/mailchimp_marketing");
 
 mailchimp.setConfig({
-  apiKey: process.env.MAILCHIMP_API_KEY,
-  server: process.env.MAILCHIMP_SERVER_PREFIX,
-})
+    apiKey: process.env.MAILCHIMP_API_KEY,
+    server: process.env.MAILCHIMP_SERVER_PREFIX,
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/inscricao.html'))
-})
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/inscricao.html"));
+});
 
-app.post('/', (req, res) => {
-  const idLista = process.env.MAILCHIMP_LIST_ID
-  const inscrito = {
-    nome: req.body.nome,
-    sobrenome: req.body.sobrenome,
-    email: req.body.email,
-  }
+app.post("/", (req, res) => {
+    const idLista = process.env.MAILCHIMP_LIST_ID;
+    const inscrito = {
+        nome: req.body.nome,
+        sobrenome: req.body.sobrenome,
+        email: req.body.email,
+    };
 
-  async function adicionarMembro() {
-    const resposta = await mailchimp.lists.addListMember(idLista, {
-      email_address: inscrito.email,
-      status: 'subscribed',
-      merge_fields: {
-        FNAME: inscrito.nome,
-        LNAME: inscrito.sobrenome,
-      },
-    })
+    async function adicionarMembro() {
+        try {
+            const resposta = await mailchimp.lists.addListMember(idLista, {
+                email_address: inscrito.email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: inscrito.nome,
+                    LNAME: inscrito.sobrenome,
+                },
+            });
 
-    console.log(
-      `Contato adicionado com sucesso. O ID do contato é ${resposta.id}.`
-    )
-  }
+            console.log(
+                `Contato adicionado com sucesso. O ID do contato é ${resposta.id}.`
+            );
 
-  adicionarMembro()
-})
+            res.sendFile(path.join(__dirname, "/sucesso.html"));
+        } catch (e) {
+            console.error(e.status);
+            res.sendFile(path.join(__dirname, "/falha.html"));
+        }
+    }
+
+    adicionarMembro();
+});
 
 app.listen(porta, () => {
-  console.log(`Servidor Express ouvindo na porta ${porta}.`)
-})
+    console.log(`Servidor Express ouvindo na porta ${porta}.`);
+});
